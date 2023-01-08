ca98cf82b9399a2607ece830b8f6e5f2271f9de4
Valerie <valerie.gleason@gmail.com>
2023-01-08 08:08:18 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './hippohonk.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Festival ratings coming soon.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
